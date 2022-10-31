132ea63d6bdf51075469e495c6bc34f95b03a40a
sudo-logic <awesomelogix@gmail.com>
2022-10-31 21:53:46 +0530

    Implemented CRUD functions

--- src/server.js
+++ src/server.js
@@ -1,30 +1,137 @@
 // Make a single RESTful API for CRUD operations in MongoDB with a
 // JavaScript server like Node.js.
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 app.use(helmet());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cors());
 
-
 const port = process.env.PORT || 5000;
 
-// Connect to the MongoDB database
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to database'));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
+const studentSchema = new mongoose.Schema(
+  {
+    name: String,
+    _id: String,
+  },
+  { versionKey: false }
+);
 
+const students = mongoose.model("Student", studentSchema);
+
+// CREATE
+
+/* Create a new student */
+app.post("/", async (req, res, next) => {
+  try {
+    const { name, _id } = req.body;
+    const student = new students({
+      name,
+      _id,
+    });
+
+    await student.save();
+    res.json(student);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// READ
+
+/* Get all students */
+app.get("/", async (req, res, next) => {
+  try {
+    const allstudents = await students.find({});
+    res.json(allstudents);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* Get a specific student */
+app.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const student = await students.findOne({
+      _id: id,
+    });
+
+    if (!student) {
+      const error = new Error("student does not exist");
+      return next(error);
+    }
+
+    res.json(student);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// UPDATE
+
+/* Update a specific student */
+app.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, _id } = req.body;
+    const student = await students.findOne({
+      _id: id,
+    });
+    if (!student) {
+      const error = new Error("student does not exist");
+      return next(error);
+    }
+    student.name = name;
+    student._id = _id;
+    await student.save();
+    res.json(student);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE
+
+/* Delete a specific student */
+app.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const student = await students.findOne({
+      _id: id,
+    });
+
+    // student does not exist
+    if (!student) {
+      return next();
+    }
+    await students.remove({
+      _id: id,
+    });
+
+    res.json({
+      message: "Success",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
